5e535467f6665508566e6e478d86cf57493608d1
aarush-goswami <2024a6r022@mietjammu.in>
2024-10-14 14:38:11 +0530

    Add files via upload

---
+++ size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    printf("size of int is %d byte\n", sizeof(int));
+    printf("size of float is %d byte\n", sizeof(float));
+    printf("size of double is %d byte\n", sizeof(double));
+    printf("size of char is %d byte\n", sizeof(char));
+    return 0;
+}
+
+
